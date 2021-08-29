0b770f5b12db7db6fe2e8b4673b49cc379170246
Jatin Rathee <jatin.rathee@razorpay.com>
2021-08-29 15:19:06 +0530

    featured refactor

---
+++ src/data/featured.js
@@ -0,0 +1,94 @@
+export default {
+  educational: {
+    list: [
+      "Assessment of Early Learning and Developmental Skills",
+      "Assessment of Educational Skills (Reading, Spelling, Writing and Math)",
+      "Assessment of Life Skills",
+    ],
+    text: "For children aged 0-5; Special Educators work on special skill sets, as outlined below. The focus of these interventions is development of language, communication skills, relational capacity and cognition.",
+    img: "1TWHcGwkAoPCV80BJkor8RXG0i9vuZLyK",
+    areasDeveloped: [
+      "Engaging",
+      "Self-Regulation",
+      "Forming relationship",
+      "Back & Forth communication",
+      "Organizing Behaviour",
+      "Problem Solving",
+      "Symbolic & Emotional Thinking",
+      "Expression of Sequencial & Thematic Play",
+    ],
+  },
+  playMovement: {
+    text: "These workshops are conducted for eight-ten weeks with a group of 5-6 children, thrice a year. The main focus of these workshops is to enhance communication & social behaviour.",
+    img: "1FkGsC6MGmgoaNuaHHIrKUzwGq76ou-_Q",
+    areasDeveloped: [
+      "Attention",
+      "Engaging and relating",
+      "Problem solving",
+      "Using words to create ideas in a group which enables social communication or non-verbal pragmatic language.",
+      "Motor Planning and sequencing",
+    ],
+    afterText:
+      "Parents are involved in these classes. Group goals have been set by the teachers according to the needs of the individual child; which are mutually agreed upon by parents and staff.",
+  },
+  danceMovement: {
+    text: "These workshops are conducted for Ten weeks with a group of 5-6 children, thrice a year. The main focus of these workshops is to enhance motor functioning and Visual Spatial processing.",
+    img: "1rCZ19O4OE2TWzBWbqyJbX4gojoBFLGs0",
+    areasDeveloped: [
+      "Motor planning",
+      "Sequencing",
+      "Left/right integration",
+      "Balance, Coordination",
+      "Body awareness and senses",
+      "Location of the body in space",
+      "Conservation of space",
+    ],
+  },
+  languageDrama: {
+    text: "Through innovative collaboration with theater artists we develop plays and dramas that creatively engage the children and their parents. Through performance arts, we are able to address the specific delay in social communication and emotional concerns.",
+    img: "19RDolKIXZfZmF9XjkoewUnhWASlxdy0c",
+    areasDeveloped: [
+      "Communication",
+      "Problem solving",
+      "Using words to create ideas",
+      "Logical thinking",
+      "Higher level of logical thinking such as ‘Multi causal thinking’ or ‘Grey area thinking’.",
+    ],
+  },
+  assistiveTechnology: {
+    text: "We have also been influenced by developments in research into child language. Research has recently focused on an area often ignored in the past; that of pragmatics, which is the study of language in its context of use. A pragmatic approach offers a perspective on child language that emphasizes how communication is achieved. It considers how language is used to communicate a variety of intentions, to relate to the communication needs of the listener and to participate in conversation and connected discourse (Bates, 1976). For addressing language and communication skills our group of experts have developed several assistive technology devices.",
+    img: "1qqu4h1CQAKike2DY89qBoEKkI8XWXLQC",
+    areasDeveloped: [
+      "Non flame cooking",
+      "Puppet making using recycled materials,",
+      "Art & Craft Workshops",
+      "Story telling & Quiz Workshop",
+      "Community Education",
+    ],
+  },
+  educationalExcursion: {
+    text: "Travelling and living beyond the familiarities of a known environment may be a challenge for many children with special needs. An annual overnight trip to a different places in WEST BENGAL is arranged, for older children to travel in a group accompanied by shumpun's staff members.",
+    img: "1flPaBvx-Pihp16GRnzXqFhFQiKFf_qKI",
+  },
+  workshops: {
+    text: "In addition to direct engagement of the children and their parents in therapy, shumpun also arranges different workshops for teachers, parents and other proffessionals. So we can say this is a platform for learning not only for the children or their families but also for community people too.",
+    img: "11lZzNqe-Q6Qjg8cHk9IDsTwxJXnsAmU0",
+    areasDeveloped: [
+      "Educational workshops for parents and other professionals caring for children with special needs. This provides a great opportunity for parents to meet and discuss with experts and address different special needs related queries, and socio-cultural issues around autism and other developmental delays in India.",
+      "Weekly professional workshops and training opportunities allow an avenue for continuing education.",
+    ],
+  },
+  camp: {
+    text: "During the summer or winter break and during the monsoon(only in weekend), we organize small therapeutic workshops for the children and their parents to develop social, motor and self-help skills through different programmes.",
+    img: "1MMrdgHrvFpULOXdm57Bj0KU8SL51xI0X",
+    activities: [
+      "Engaging in the group",
+      "Cooperation",
+      "Following instruction",
+      "Flexibility",
+      "Inter-Personal-Social interaction",
+      "Understanding perspective of Others",
+      "Adoptability",
+    ],
+  },
+};
--- src/sections/featured.js
+++ src/sections/featured.js
@@ -5,113 +5,11 @@ import BlockTitle from "components/block-title";
 import { FaSchool } from "react-icons/fa";
 import dotPattern from "assets/dot-pattern.svg";
 
-import educational from "assets/featured/educational.jpeg";
-import playMovement from "assets/featured/playMovement.jpeg";
-import danceMovement from "assets/featured/danceMovement.jpeg";
-import languageDrama from "assets/featured/languageDrama.jpeg";
-import assistiveTechnology from "assets/featured/assistiveTechnology.jpeg";
-import educationalExcursion from "assets/featured/educationalExcursion.jpeg";
-import workshops from "assets/featured/workshops.jpeg";
-import camp from "assets/featured/camp.jpeg";
+import dataMap from "data/featured";
 
 const getLabel = (text) => {
   const result = text.replace(/([A-Z])/g, " $1");
   return result.charAt(0).toUpperCase() + result.slice(1);
-};
-
-const dataMap = {
-  educational: {
-    list: [
-      "Assessment of Early Learning and Developmental Skills",
-      "Assessment of Educational Skills (Reading, Spelling, Writing and Math)",
-      "Assessment of Life Skills",
-    ],
-    text: "For children aged 0-5; Special Educators work on special skill sets, as outlined below. The focus of these interventions is development of language, communication skills, relational capacity and cognition.",
-    img: educational,
-    areasDeveloped: [
-      "Engaging",
-      "Self-Regulation",
-      "Forming relationship",
-      "Back & Forth communication",
-      "Organizing Behaviour",
-      "Problem Solving",
-      "Symbolic & Emotional Thinking",
-      "Expression of Sequencial & Thematic Play",
-    ],
-  },
-  playMovement: {
-    text: "These workshops are conducted for eight-ten weeks with a group of 5-6 children, thrice a year. The main focus of these workshops is to enhance communication & social behaviour.",
-    img: playMovement,
-    areasDeveloped: [
-      "Attention",
-      "Engaging and relating",
-      "Problem solving",
-      "Using words to create ideas in a group which enables social communication or non-verbal pragmatic language.",
-      "Motor Planning and sequencing",
-    ],
-    afterText:
-      "Parents are involved in these classes. Group goals have been set by the teachers according to the needs of the individual child; which are mutually agreed upon by parents and staff.",
-  },
-  danceMovement: {
-    text: "These workshops are conducted for Ten weeks with a group of 5-6 children, thrice a year. The main focus of these workshops is to enhance motor functioning and Visual Spatial processing.",
-    img: danceMovement,
-    areasDeveloped: [
-      "Motor planning",
-      "Sequencing",
-      "Left/right integration",
-      "Balance, Coordination",
-      "Body awareness and senses",
-      "Location of the body in space",
-      "Conservation of space",
-    ],
-  },
-  languageDrama: {
-    text: "Through innovative collaboration with theater artists we develop plays and dramas that creatively engage the children and their parents. Through performance arts, we are able to address the specific delay in social communication and emotional concerns.",
-    img: languageDrama,
-    areasDeveloped: [
-      "Communication",
-      "Problem solving",
-      "Using words to create ideas",
-      "Logical thinking",
-      "Higher level of logical thinking such as ‘Multi causal thinking’ or ‘Grey area thinking’.",
-    ],
-  },
-  assistiveTechnology: {
-    text: "We have also been influenced by developments in research into child language. Research has recently focused on an area often ignored in the past; that of pragmatics, which is the study of language in its context of use. A pragmatic approach offers a perspective on child language that emphasizes how communication is achieved. It considers how language is used to communicate a variety of intentions, to relate to the communication needs of the listener and to participate in conversation and connected discourse (Bates, 1976). For addressing language and communication skills our group of experts have developed several assistive technology devices.",
-    img: assistiveTechnology,
-    areasDeveloped: [
-      "Non flame cooking",
-      "Puppet making using recycled materials,",
-      "Art & Craft Workshops",
-      "Story telling & Quiz Workshop",
-      "Community Education",
-    ],
-  },
-  educationalExcursion: {
-    text: "Travelling and living beyond the familiarities of a known environment may be a challenge for many children with special needs. An annual overnight trip to a different places in WEST BENGAL is arranged, for older children to travel in a group accompanied by shumpun's staff members.",
-    img: educationalExcursion,
-  },
-  workshops: {
-    text: "In addition to direct engagement of the children and their parents in therapy, shumpun also arranges different workshops for teachers, parents and other proffessionals. So we can say this is a platform for learning not only for the children or their families but also for community people too.",
-    img: workshops,
-    areasDeveloped: [
-      "Educational workshops for parents and other professionals caring for children with special needs. This provides a great opportunity for parents to meet and discuss with experts and address different special needs related queries, and socio-cultural issues around autism and other developmental delays in India.",
-      "Weekly professional workshops and training opportunities allow an avenue for continuing education.",
-    ],
-  },
-  camp: {
-    text: "During the summer or winter break and during the monsoon(only in weekend), we organize small therapeutic workshops for the children and their parents to develop social, motor and self-help skills through different programmes.",
-    img: camp,
-    activities: [
-      "Engaging in the group",
-      "Cooperation",
-      "Following instruction",
-      "Flexibility",
-      "Inter-Personal-Social interaction",
-      "Understanding perspective of Others",
-      "Adoptability",
-    ],
-  },
 };
 
 const TabContent = ({ type }) => {
@@ -129,7 +27,10 @@ const TabContent = ({ type }) => {
         </ul>
       )}
       <Text as="p">{text}</Text>
-      <Image src={img} sx={styles.tabImage} />
+      <Image
+        src={`https://drive.google.com/uc?export=view&id=${img}`}
+        sx={styles.tabImage}
+      />
       {areasDeveloped && (
         <>
           <Heading as="h3" sx={{ mt: "20px" }}>
